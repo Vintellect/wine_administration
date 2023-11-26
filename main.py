0cd46e977cec2855aeb494b50f545caba91eef6d
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2023-11-26 10:23:10 +0100

    Add: is_admin func using user_microservices

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ instance_id = os.getenv("SPANNER_INSTANCE")
 database_id = os.getenv("SPANNER_DATABASE")
 # [END create_spanner]
 
+def is_admin(token):
+    response = requests.get(user_microservices +"/isAdmin", params={'token': token})
+    if response.status_code == 200:
+        return response.json().get('is_admin', False)
+    else:
+        print(f"Failed to check admin status. Status code: {response.status_code}")
+        return False
 
 
 # [END app]
